a02bff3cde71ace4a657c6d701f090f3e667250d
bbindae <bbindae@hotmail.com>
2016-10-03 20:40:09 -0700

    change Money to decimal

--- ShoppingCart/src/ShoppingCart/ProductCatalogClient.cs
+++ ShoppingCart/src/ShoppingCart/ProductCatalogClient.cs
@@ -35,7 +35,7 @@ namespace ShoppingCart
             public string ProductId {get;set;}
             public string ProductName {get; set;}
             public string ProductDescription {get; set;}
-            public Money Price {get; set;}
+            public decimal Price {get; set;}
         }
     }
 }
